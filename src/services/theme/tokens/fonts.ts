4e86005358605325f1b969264daaa8b9532bf4ec
Mark Harper <markharper20@gmail.com>
2023-05-01 18:45:11 -0500

    remove stalemate font

--- src/services/theme/tokens/fonts.ts
+++ src/services/theme/tokens/fonts.ts
@@ -1,5 +1,4 @@
 export const FONTS = {
-  fancy: `'Stalemate', cursive`,
   official: `'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode'
    Geneva, Verdana, sans-serif`,
   nav: 'palatino, sans-serif',
